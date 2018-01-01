1a1a433020ee06fa06e7c652810bcc4fc606bc6c
James Denman <james@fastmail.se>
2017-12-31 22:17:07 -0500

    force

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-expected="Hello World"
-actual=`nc -v localhost 9080`
+expected="<title>WebSphere Liberty 17.0.0.4</title>"
+actual=`curl localhost`
 echo "Expecting: $expected"
 echo "Server says: $actual"
-if [ "$expected" != "$actual" ]; then
+if [[ "$expected" == *"$actual"* ]]; then
   echo "Test failed"
   exit 1
 else
